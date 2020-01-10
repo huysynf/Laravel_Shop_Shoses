0540364b1b7055d32a68e9047f73cea8c23140db
haqhuy99 <haqhuy99.wd@gmail.com>
2020-01-10 13:38:01 +0700

    edit file category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'parent_id',
+    ];
+
+    public function parent()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function childOf()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
