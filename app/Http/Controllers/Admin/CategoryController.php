824f38f6ae41449f854c1ab03cc86de62282d999
haqhuy99 <haqhuy99.wd@gmail.com>
2020-01-11 15:25:04 +0700

    craete function crate category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,24 +3,37 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\admin\CategoryRepository;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+    protected $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+
+        $this->categoryRepository = $categoryRepository;
+    }
 
     public function index()
     {
         return view('admins.categories.index');
     }
 
-    public function create()
-    {
-        //
+    public function formatRequest(Request $request){
+       return $this->categoryRepository->formatRequest($request);
     }
 
     public function store(Request $request)
     {
-        //
+        $data=$this->formatRequest($request);
+        $category=$this->categoryRepository->store($data);
+        return response()->json([
+           'status'=>200,
+           'message'=>'Thêm thành công danh mục: '.$category->name,
+        ]);
     }
 
     public function show($id)
@@ -43,4 +56,43 @@ class CategoryController extends Controller
     {
         //
     }
+}
+
+
+function conwayGame($a,$b)
+{
+    $arr=[];
+    $text=$a." ".$b."\n";
+    for($i=0;$i<$a;$i++){
+
+        for($j=0;$j<$b;$j++){
+
+
+            if ($i>0 && $i<3 && $j>2 && $j <5)
+            {
+
+                $arr[$i][$j]="*";
+
+            }
+            else
+            {
+                $arr[$i][$j]=".";
+            }
+
+        }
+    }
+    for($i=0;$i<$a;$i++){
+
+        for($j=0;$j<$b;$j++){
+
+            $text.=$arr[$i][$i]." ";
+
+        }
+
+        $text.="\n";
+
+    }
+
+
+return $text;
 }
--- app/Repositories/admin/CategoryRepository.php
+++ app/Repositories/admin/CategoryRepository.php
@@ -3,10 +3,27 @@
 namespace App\Repositories\admin;
 
 
+use App\Models\Category;
 use App\Repositories\BaseRepository;
-
+use Illuminate\Http\Request;
 
 class CategoryRepository extends BaseRepository
 {
+    public function __construct(Category $category)
+    {
+        $this->model=$category;
+    }
+    public function formatRequest(Request $request):array {
+        $data=$request->all();
+        $data['slug']=$data['name'];
+        return  $data;
+    }
+
+    public function store(array $data)
+    {
+        $category= $this->model->create($data);
+        return $category;
+    }
+
 
 }
