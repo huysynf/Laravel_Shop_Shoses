27d1954be9d1929affb8c9a98460199b02e34a24
haqhuy99 <haqhuy99.wd@gmail.com>
2020-01-31 16:20:01 +0700

    add policy to view

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,24 @@ Route::get('/', function () {
 });
 
 Auth::routes(
-   [
-       'register' => false,
-       'verify' => true,
-       'reset' => false,
-       'login' => false,
-   ]
+    [
+        'register' => false,
+        'verify' => true,
+        'reset' => false,
+        'login' => false,
+    ]
 );
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'manage','middleware'=>'auth'], function () {
-    include_route_files(__DIR__.'/admin/');
+Route::group(['namespace' => 'Admin', 'prefix' => 'manage', 'middleware' => 'auth'], function () {
+    include_route_files(__DIR__ . '/admin/');
 });
 
 Route::group(['namespace' => 'Auth', 'prefix' => 'manage'], function () {
-    Route::get('/login','LoginController@showLoginForm')->name('manage.getLogin');
-    Route::post('/login','LoginController@Login')->name('manage.login');
+    Route::get('/login', 'LoginController@showLoginForm')->name('manage.getLogin');
+    Route::post('/login', 'LoginController@Login')->name('manage.login');
 });
-Route::get('/login',function (){
-   abort(404);
+Route::get('/login', function () {
+    abort(404);
 });
