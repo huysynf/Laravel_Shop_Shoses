5677711844b2db7222af528fdc8c272291791460
haqhuy99 <haqhuy99.wd@gmail.com>
2020-02-26 08:50:15 +0700

    creae event

---
+++ app/Listeners/SentEmailAfterOrderPayment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPayment;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SentEmailAfterOrderPayment
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  =OrderPayment  $event
+     * @return void
+     */
+    public function handle(OrderPayment $event)
+    {
+        //
+    }
+}
