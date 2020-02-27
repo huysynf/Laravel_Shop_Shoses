5c9b9c7fe6a2db79522ee5e1475446b00ee2e608
haqhuy99 <haqhuy99.wd@gmail.com>
2020-02-27 09:07:49 +0700

    fix

---
+++ app/Observers/AccountObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\=User;
+
+class AccountObserver
+{
+    /**
+     * Handle the = user "created" event.
+     *
+     * @param  \App\=User  $=User
+     * @return void
+     */
+    public function created(=User $=User)
+    {
+        //
+    }
+
+    /**
+     * Handle the = user "updated" event.
+     *
+     * @param  \App\=User  $=User
+     * @return void
+     */
+    public function updated(=User $=User)
+    {
+        //
+    }
+
+    /**
+     * Handle the = user "deleted" event.
+     *
+     * @param  \App\=User  $=User
+     * @return void
+     */
+    public function deleted(=User $=User)
+    {
+        //
+    }
+
+    /**
+     * Handle the = user "restored" event.
+     *
+     * @param  \App\=User  $=User
+     * @return void
+     */
+    public function restored(=User $=User)
+    {
+        //
+    }
+
+    /**
+     * Handle the = user "force deleted" event.
+     *
+     * @param  \App\=User  $=User
+     * @return void
+     */
+    public function forceDeleted(=User $=User)
+    {
+        //
+    }
+}
