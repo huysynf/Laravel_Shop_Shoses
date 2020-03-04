3642b2df15eb940f8a9cb6225c72795f294d2b9d
haqhuy99 <haqhuy99.wd@gmail.com>
2020-03-04 13:57:18 +0700

    creae login with facebook

--- routes/auth/google.php
+++
@@ -1,3 +0,0 @@
-<?php
-Route::get('/auth/google', 'LoginWithGoogleController@redirect')->name('login.google');
-Route::get('/auth/google/callback', 'LoginWithGoogleController@callback')->name('login.google_callback');
